281d5cda3d2831a59e508993f46a105abf987946
Al Zee <z@alz.ee>
2025-10-28 16:14:45 +0800

    nth

--- bin/container-test.sh
+++ bin/container-test.sh
@@ -11,3 +11,7 @@ rank_id_start=0
 cd /usr/local/Ascend/atb-models/tests/modeltest/
 
 bash run.sh pa_bf16 full_CEval 5 1 deepseekv2 /data2/hf/models/deepseek-v3-bf16/ /data/rank_table.json 32 4 $rank_id_start $master_address
+
+# bash run.sh pa_bf16 full_GSM8K 8 deepseekv2 /data2/hf/models/deepseek-v3-bf16/ /data/rank_table.json 32 4 $rank_id_start $master_address
+
+# bash run.sh pa_bf16 performance [[256,256]] 1 deepseekv2 /data2/hf/models/deepseek-v3-bf16 /data/rank_table.json 32 4 $rank_id_start $master_address
