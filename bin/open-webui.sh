25fbe3b3b08e9d5e52a1c8624531580d8efe9c4d
Al Zee <z@alz.ee>
2025-11-05 15:59:53 +0800

    nth

--- bin/open-webui.sh
+++ bin/open-webui.sh
@@ -4,13 +4,10 @@
 
 # see https://docs.openwebui.com/getting-started/quick-start/
 
-
 docker pull ghcr.io/open-webui/open-webui:main
 
 image=ghcr.io/open-webui/open-webui:main
 name=open-webui
 volume=open-webui
 
-export DEFAULT_LOCALE=zh-CN
-
-docker run -d -p 3000:8080 -v $volume:/app/backend/data --name $name $image
+docker run -e DEFAULT_LOCALE=zh-CN -d -p 3000:8080 -v $volume:/app/backend/data --name $name $image
