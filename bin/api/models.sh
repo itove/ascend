2aaca23c47c836eff36b5a25a6442dee24cee2ec
Al Zee <z@alz.ee>
2025-11-11 16:24:10 +0800

    nth

---
+++ bin/api/models.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# vim:ft=bash
+
+. .env.local
+
+# URL=https://ai.zxaicc.com
+URL=https://172.20.29.123
+# URL=gx1:1025
+
+#--header 'Authorization: Bearer API-KEY' \
+# ip为主节点ip,port为主节点port
+curl -k --location "$URL/api/models" \
+--header "Authorization: Bearer $API_KEY" \
+--header 'Content-Type: application/json' 
+
+echo
--- bin/open-webui.sh
+++ bin/open-webui.sh
@@ -4,10 +4,17 @@
 
 # see https://docs.openwebui.com/getting-started/quick-start/
 
-docker pull ghcr.io/open-webui/open-webui:main
+#docker pull ghcr.io/open-webui/open-webui:main
 
 image=ghcr.io/open-webui/open-webui:main
 name=open-webui
 volume=open-webui
 
-docker run -e DEFAULT_LOCALE=zh-CN -d -p 3000:8080 -v $volume:/app/backend/data --name $name $image
+docker run \
+    -e DEFAULT_LOCALE=zh-CN \
+    -e ENABLE_OPENAI_API=True
+    -e OPENAI_API_BASE_URL=http://172.20.84.67:1025/v1 \
+    -d -p 3000:8080 \
+    -v $volume:/app/backend/data \
+    --name $name \
+    $image
