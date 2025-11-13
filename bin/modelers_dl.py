abba675623fe7cdae579820bdf8c1bde6afaf613
Al Zee <z@alz.ee>
2025-11-13 17:12:00 +0800

    uv support

---
+++ bin/modelers_dl.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+from openmind_hub import snapshot_download
+
+snapshot_download(
+    repo_id="Modelers_Park/DeepSeek-R1-0528-w8a8",
+    local_dir="/mnt/d/models/modelers/Modelers_Park/DeepSeek-R1-0528-w8a8",
+    local_dir_use_symlinks=False
+)
