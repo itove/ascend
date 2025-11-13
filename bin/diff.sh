eb38fe233f82cdfe870b087ef8c5d8331cfc534a
Al Zee <z@alz.ee>
2025-11-13 21:37:51 +0800

    nth

--- bin/diff.sh
+++ bin/diff.sh
@@ -2,5 +2,9 @@
 #
 # vim:ft=bash
 
+if [ -z "$1"]; then
+    echo 'Usage: bin/diff.sh <target_dir>'
+    exit
+fi
 
 diff -r -x '*.safetensors' -x '.cache' -x '.git' -x '__pycache__' -x 'verify.log' -x '.gitignore' . "$1"
