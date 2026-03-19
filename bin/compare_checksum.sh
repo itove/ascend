34708a2efb0c8893ab377be52109069669a67424
Al Zee <z@alz.ee>
2026-03-19 10:54:43 +0800

    nth

--- bin/compare_checksum.sh
+++ bin/compare_checksum.sh
@@ -3,8 +3,35 @@
 # vim:ft=bash
 
 ############### Variables ###############
+checksum_file=checksum
 
 ############### Functions ###############
+is_lfs_pointer(){
+    local ret
+    ret=-1
+    if [ "$1" = "ASCII text" ]; then
+        if head -n 1 "$1" | grep -q "git-lfs.github.com/spec"; then
+            ret=0
+        fi
+    fi
+
+    return $ret
+}
+
+checksum(){
+    files=`find . \( -path './.git' -o -path './.cache' \) -prune -o -type f -print`
+
+    for i in $files
+    do
+        if is_lfs_pointer "$i"; then
+            checksum=$(grep sha256 "$i")
+            checksum=${checksum#*:}
+        else
+            echo Computing sha256sum for $i
+            checksum=$(sha256sum "$i" | awk '{print $1}')
+        fi
+    done
+}
 
 ############### Main Part ###############
 
@@ -13,24 +40,7 @@ if [ -z "$1" ]; then
     exit
 fi
 
-checksum_file=checksum
 rm -f $checksum_file "$1"/$checksum_file
-
-checksum(){
-    files=`find . \( -path './.git' -o -path './.cache' \) -prune -o -type f -print`
-
-    for i in $files
-    do
-        # if $i is "ASCII text" AND is a lfs
-        if :; then
-            checksum=$(grep sha256 $i)
-            checksum=${checksum#*:}
-        else
-            echo Computing sha256sum for $i
-            checksum=$(sha256sum $i | awk '{print $1}')
-        fi
-    done
-}
 
 # compute file sums in current dir
 checksum
