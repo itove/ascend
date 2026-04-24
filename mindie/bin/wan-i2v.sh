752bf660f080f261aaf023887543306ff308e4e3
Al Zee <z@alz.ee>
2026-04-24 10:41:38 +0800

    nth

--- mindie/bin/wan-i2v.sh
+++ mindie/bin/wan-i2v.sh
@@ -13,18 +13,37 @@ export FAST_LAYERNORM=0
 
 prompt="${1:-young woman 21 Years old with long curly  dark hair, vibrante Red lips is dancing  mówimy hips in a bright, old styl cowboy salon. She wears a  jeans, black top and cowboy boots, with minimal makeup. Her dance is sofisticacet  city street, and frequent eye contact with the camera. The camera is vertical 9:16, mostly medium shots with occasional close-ups, slightly panning and zooming to match the beat. The background has colorful neon lights or pastel walls, and the lighting pulses slightly to the music.}"
 
+image="$2"
+
+if [ -z "$image" ]; then
+    echo Usage: $(basename $0) '<prompt>' '<image_path>'
+    exit
+fi
+
 echo Prompt: "$prompt"
 
-torchrun --master_port=23459 --nproc_per_node=8 generate.py \
-    --ulysses_size 8 \
-    --cfg_size 1 \
-    --task t2v-A14B \
-    --ckpt_dir ${model_base} \
+# Multi-GPU inference using FSDP + DeepSpeed Ulysses
+torchrun --nproc_per_node=8 generate.py \
+    --task i2v-A14B \
     --size 1280*720 \
-    --frame_num 81 \
-    --sample_steps 50 \
+    --ckpt_dir ${model_base} \
+    --image examples/i2v_input.JPG \
     --dit_fsdp \
     --t5_fsdp \
-    --vae_parallel \
-    --prompt "$prompt" \
-    --base_seed 0
+    --cfg_size 1 \
+    --ulysses_size 8 \
+    --prompt "$prompt"
+
+# Image-to-Video Generation without prompt
+#DASH_API_KEY=your_key torchrun --nproc_per_node=8 generate.py \
+#    --task i2v-A14B \
+#    --size 1280*720 \
+#    --ckpt_dir ${model_base} \
+#    --prompt '' \
+#    --image examples/i2v_input.JPG \
+#    --dit_fsdp \
+#    --t5_fsdp \
+#    --cfg_size 1 \
+#    --ulysses_size 8 \
+#    --use_prompt_extend \
+#    --prompt_extend_method 'dashscope'
