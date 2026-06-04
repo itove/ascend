51ee88146fcfe395addb22a5a555d1aa51078d79
Al Zee <z@alz.ee>
2026-06-04 15:17:18 +0800

    nth

---
+++ vllm/bin/serve-qwen3.6-35b-a3b.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# vim:ft=bash
+
+set -e
+
+. /s/ascend/vllm/ENVs
+# Load model from ModelScope to speed up download
+export VLLM_USE_MODELSCOPE=True
+# To reduce memory fragmentation and avoid out of memory
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+export HCCL_OP_EXPANSION_MODE="AIV"
+export HCCL_BUFFSIZE=1024
+export OMP_NUM_THREADS=1
+export TASK_QUEUE_ENABLE=1
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0
+sysctl -w kernel.numa_balancing=0
+sysctl kernel.sched_migration_cost_ns=50000
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so.2:$LD_PRELOAD
+export VLLM_ASCEND_ENABLE_FLASHCOMM1=1
+
+unset USE_MULTI_BLOCK_POOL
+unset OMP_PROC_BIND
+unset ACL_OP_INIT_MODE
+unset TRITON_ALL_BLOCKS_PARALLEL
+
+vllm serve $MODEL_PATH \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --data-parallel-size 1 \
+    --tensor-parallel-size 2 \
+    --enable-expert-parallel \
+    --seed 1024 \
+    --quantization ascend \
+    --served-model-name $MODEL_NAME \
+    --max-num-seqs 128 \
+    --max-model-len 262144 \
+    --max-num-batched-tokens 16384 \
+    --trust-remote-code \
+    --gpu-memory-utilization 0.90 \
+    --enable-prefix-caching \
+    --speculative_config '{"method": "qwen3_5_mtp", "num_speculative_tokens": 3, "enforce_eager": true}' \
+    --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}' \
+    --additional-config '{"enable_cpu_binding":true, "multistream_overlap_shared_expert": true}' \
+    --async-scheduling
