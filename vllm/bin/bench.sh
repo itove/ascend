245eef09c353fe63a602392f8260855011f25cc7
Al Zee <z@alz.ee>
2026-06-08 15:30:32 +0800

    nth

--- vllm/bin/bench.sh
+++ vllm/bin/bench.sh
@@ -6,11 +6,15 @@
 
 export VLLM_USE_MODELSCOPE=true
 
+host=${1:-127.0.0.1}
+port=${2:-8006}
+
 # see https://docs.vllm.ai/en/latest/cli/bench/serve/
 vllm bench serve \
     --model $MODEL_PATH \
     --served-model-name $MODEL_NAME \
-    --port 8006 \
+    --host $host \
+    --port $port \
     --backend openai-chat \
     --endpoint /v1/chat/completions \
     --dataset-name random \
