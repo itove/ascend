9224eaa2271786bde68a2e6e5f2f333a8e020555
Al Zee <z@alz.ee>
2026-06-29 09:55:04 +0800

    nth

--- vllm/bin/serve-qwen3-asr-1.7b.sh
+++ vllm/bin/serve-qwen3-asr-1.7b.sh
@@ -9,8 +9,9 @@ set -e
 export ASCEND_RT_VISIBLE_DEVICES="4"
 
 vllm serve $MODEL_PATH \
-  --tensor-parallel-size 1 \
-  --max-model-len 4096 \
-  --gpu-memory-utilization 0.9 \
-  --enforce-eager \
-  --port 8003
+    --served-model-name $MODEL_NAME \
+    --tensor-parallel-size 1 \
+    --max-model-len 4096 \
+    --gpu-memory-utilization 0.9 \
+    --enforce-eager \
+    --port 8003
