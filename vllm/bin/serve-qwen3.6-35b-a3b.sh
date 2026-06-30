199470bfc201a65369f7c1bf89fd5568d3c675b1
Al Zee <z@alz.ee>
2026-06-30 11:30:17 +0800

    nth

--- vllm/bin/serve-qwen3.6-35b-a3b.sh
+++ vllm/bin/serve-qwen3.6-35b-a3b.sh
@@ -43,4 +43,6 @@ vllm serve $MODEL_PATH \
     --speculative_config '{"method": "qwen3_5_mtp", "num_speculative_tokens": 3, "enforce_eager": true}' \
     --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}' \
     --additional-config '{"enable_cpu_binding":true, "ascend_scheduler_config": {"enabled": true}, "enable_prefill_optimizations": true, "multistream_overlap_shared_expert": true}' \
+    --tool-call-parser "qwen3_coder" \
+    --enable-auto-tool-choice \
     --async-scheduling
