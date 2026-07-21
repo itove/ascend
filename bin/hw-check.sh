368cd29b99f26c5a255163bd790a4a6a897b6371
Al Zee <z@alz.ee>
2026-07-21 15:42:12 +0800

    nth

--- bin/hw-check.sh
+++ bin/hw-check.sh
@@ -12,14 +12,16 @@
 for i in {0..7}
 do
     echo Checking $i
-    hccn_tool -i $i -ip -g
-    hccn_tool -i $i -ping -g address 172.20.60.1
-    hccn_tool -i $i -lldp -g | grep Ifname
-    hccn_tool -i $i -link -g
-    hccn_tool -i $i -net_health -g
-    hccn_tool -i $i -netdetect -g
-    hccn_tool -i $i -gateway -g
-    sudo hccn_tool -i $i -tls -s enable 0
-    sudo hccn_tool -i $i -tls -g | grep switch
+
+    sudo hccn_tool -i $i -ip -g
+    sudo hccn_tool -i $i -ping -g address 172.20.60.1
+
+    sudo hccn_tool -i $i -lldp -g | grep Ifname
+    sudo hccn_tool -i $i -link -g
+    sudo hccn_tool -i $i -net_health -g
+    sudo hccn_tool -i $i -netdetect -g
+    sudo hccn_tool -i $i -gateway -g
+    # sudo hccn_tool -i $i -tls -s enable 0
+    # sudo hccn_tool -i $i -tls -g | grep switch
     echo
 done
