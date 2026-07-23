5b8b6398bf689b3acddedf006d8878442cd3d3d8
Al Zee <z@alz.ee>
2026-07-23 15:03:46 +0800

    nth

--- vllm/pd_disaggregation/qwen3.6-27b-w8a8/prefill/run_dp_template.sh
+++ vllm/pd_disaggregation/qwen3.6-27b-w8a8/prefill/run_dp_template.sh
@@ -55,11 +55,11 @@ vllm serve $MODEL_PATH \
   "engine_id": "0",
   "kv_connector_extra_config": {
             "prefill": {
-                    "dp_size": 8,
+                    "dp_size": 4,
                     "tp_size": 2
             },
             "decode": {
-                    "dp_size": 8,
+                    "dp_size": 4,
                     "tp_size": 2
         }
     }
