cbc8e782e1cbc3031cbb252507e2fb300407181e
Al Zee <z@alz.ee>
2026-07-23 16:32:13 +0800

    nth

--- vllm/pd_disaggregation/bin/launch_online_dp_prefill.sh
+++ vllm/pd_disaggregation/bin/launch_online_dp_prefill.sh
@@ -7,4 +7,4 @@
 ############### Functions ###############
 
 ############### Main Part ###############
-python launch_online_dp.py --dp-size 16 --tp-size 2 --dp-size-local 4 --dp-rank-start 0 --dp-address 172.20.84.145 --dp-rpc-port 12321 --vllm-start-port 7100
+python /vllm-workspace/vllm-ascend/examples/external_online_dp/launch_online_dp.py --dp-size 16 --tp-size 2 --dp-size-local 4 --dp-rank-start 0 --dp-address 172.20.84.145 --dp-rpc-port 12321 --vllm-start-port 7100
