c61cb572a358ac8a685d1970eed1c72b529b2352
Al Zee <z@alz.ee>
2026-07-23 18:05:49 +0800

    nth

---
+++ vllm/pd_disaggregation/qwen3.6-35b-a3b/prefill/launch_online_dp_prefill.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# vim:ft=bash
+
+############### Variables ###############
+
+############### Functions ###############
+
+############### Main Part ###############
+python /vllm-workspace/vllm-ascend/examples/external_online_dp/launch_online_dp.py --dp-size 16 --tp-size 2 --dp-size-local 4 --dp-rank-start 0 --dp-address 172.20.84.145 --dp-rpc-port 12321 --vllm-start-port 7100
