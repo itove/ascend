82ddf92443030cac4dabd68e2332c708bd4cf688
Al Zee <z@alz.ee>
2026-07-24 11:31:42 +0800

    nth

--- vllm/pd_disaggregation/qwen3.6-27b-w8a8/prefill/launch_online_dp_prefill.sh
+++ vllm/pd_disaggregation/qwen3.6-27b-w8a8/prefill/launch_online_dp_prefill.sh
@@ -6,6 +6,6 @@ nic_name="bond0.284"
 local_ip=$(ifconfig "$nic_name" | awk '/inet /{print $2}')
 head_node_ip="172.20.84.145"
 
-export MODEL_PATH=/s/hf/Qwen/Qwen3.6-35B-A3B
+export MODEL_PATH=/s/modelscope/Eco-Tech/Qwen3.6-27B-w8a8
 
 python /vllm-workspace/vllm-ascend/examples/external_online_dp/launch_online_dp.py --dp-size 16 --tp-size 2 --dp-size-local 4 --dp-rank-start 0 --dp-address $head_node_ip --dp-rpc-port 12321 --vllm-start-port 7100
