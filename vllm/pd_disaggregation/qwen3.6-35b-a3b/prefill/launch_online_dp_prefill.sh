114db19d528d27a1235cec4a3490669ff61cbee8
Al Zee <z@alz.ee>
2026-07-24 10:48:37 +0800

    nth

--- vllm/pd_disaggregation/qwen3.6-35b-a3b/prefill/launch_online_dp_prefill.sh
+++ vllm/pd_disaggregation/qwen3.6-35b-a3b/prefill/launch_online_dp_prefill.sh
@@ -4,6 +4,6 @@
 
 nic_name="bond0.284"
 local_ip=$(ifconfig "$nic_name" | awk '/inet /{print $2}')
-master_node_ip="172.20.84.145"
+head_node_ip="172.20.84.145"
 
-python /vllm-workspace/vllm-ascend/examples/external_online_dp/launch_online_dp.py --dp-size 16 --tp-size 2 --dp-size-local 4 --dp-rank-start 0 --dp-address $master_node_ip --dp-rpc-port 12321 --vllm-start-port 7100
+python /vllm-workspace/vllm-ascend/examples/external_online_dp/launch_online_dp.py --dp-size 16 --tp-size 2 --dp-size-local 4 --dp-rank-start 0 --dp-address $head_node_ip --dp-rpc-port 12321 --vllm-start-port 7100
