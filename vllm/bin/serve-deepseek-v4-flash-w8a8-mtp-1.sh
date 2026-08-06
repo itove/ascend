f2846e2fc3fb55ab509e18bed852d05e662f7d43
Al Zee <z@alz.ee>
2026-08-06 14:34:55 +0800

    nth

---
+++ vllm/bin/serve-deepseek-v4-flash-w8a8-mtp-1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# vim:ft=bash
+
+set -e
+
+. /s/ascend/vllm/ENVs
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so.2:$LD_PRELOAD
+export OMP_PROC_BIND=false
+export OMP_NUM_THREADS=10
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+export HCCL_BUFFSIZE=1024
+export TASK_QUEUE_ENABLE=1
+export HCCL_OP_EXPANSION_MODE="AIV"
+
+vllm serve $MODEL_PATH \
+    --max-model-len 133120 \
+    --max-num-batched-tokens 8192 \
+    --served-model-name $MODEL_NAME \
+    --gpu-memory-utilization 0.9 \
+    --max-num-seqs 32 \
+    --data-parallel-size 1 \
+    --tensor-parallel-size 8 \
+    --enable-expert-parallel \
+    --tokenizer-mode deepseek_v4 \
+    --tool-call-parser deepseek_v4 \
+    --enable-auto-tool-choice \
+    --reasoning-parser deepseek_v4 \
+    --no-enable-prefix-caching \
+    --model-loader-extra-config='{"enable_multithread_load": true, "num_threads": 128}' \
+    --quantization ascend \
+    --port 8900 \
+    --block-size 128 \
+    --speculative-config '{"num_speculative_tokens": 1,"method": "mtp","enforce_eager": true}' \
+    --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY"}' \
+    --additional-config '
+    {"ascend_compilation_config":{
+        "enable_npugraph_ex":true,
+        "enable_static_kernel":false
+        },
+    "enable_cpu_binding": true,
+    "enable_dsa_cp": true,
+    "enable_flashcomm1": true,
+    "multistream_overlap_shared_expert": true}'
