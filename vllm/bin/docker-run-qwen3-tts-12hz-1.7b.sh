32124589b5fae0cebc503c0e4b41b832bdc11fc8
Al Zee <z@alz.ee>
2026-08-18 18:05:45 +0800

    nth

--- vllm/bin/docker-run-qwen3-tts-12hz-1.7b.sh
+++ vllm/bin/docker-run-qwen3-tts-12hz-1.7b.sh
@@ -22,8 +22,15 @@ docker run --rm \
     --privileged \
     --name $name \
     --net=host \
-    --shm-size=1g \
+    --shm-size=512g \
+    --device /dev/davinci0 \
+    --device /dev/davinci1 \
+    --device /dev/davinci2 \
+    --device /dev/davinci3 \
+    --device /dev/davinci4 \
     --device /dev/davinci5 \
+    --device /dev/davinci6 \
+    --device /dev/davinci7 \
     --device /dev/davinci_manager \
     --device /dev/devmm_svm \
     --device /dev/hisi_hdc \
