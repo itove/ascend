c1f5c54e2ab6435310314b6c6f5666ae3eb3d0cb
Al Zee <z@alz.ee>
2026-08-18 16:55:46 +0800

    nth

--- vllm/bin/serve-qwen3-tts-12hz-1.7b.sh
+++ vllm/bin/serve-qwen3-tts-12hz-1.7b.sh
@@ -10,6 +10,7 @@ export ASCEND_RT_VISIBLE_DEVICES="5"
 
     #--deploy-config vllm_omni/deploy/qwen3_tts.yaml \
 vllm serve $MODEL_PATH \
+    --allowed-local-media-path \
     --served-model-name $MODEL_NAME \
     --omni \
     --port 8004 \
