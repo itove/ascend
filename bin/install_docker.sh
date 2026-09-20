3efac679032c151aeb81d0a7f231d18c614df1e0
Al Zee <z@alz.ee>
2026-09-20 10:51:16 +0800

    nth

--- bin/install_docker.sh
+++ bin/install_docker.sh
@@ -4,10 +4,14 @@
 # Install docker on openEuler 22.03 LTS / 5.10.0-60.18.0.50.oe2203.aarch64
 
 sudo dnf -y install dnf-plugins-core
+# use rhel repo
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+# or centos
+# sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Edit `/etc/yum.repos.d/docker-ce.repo`, change `$releasever` to `10`.
-$ sudo sed -i.bak s/\$releasever/10/ /etc/yum.repos.d/docker-ce.repo
+# Edit `/etc/yum.repos.d/docker-ce.repo`, change `$releasever` to `8`.
+# docker on 9/10 requires glibc 2.38, but glibc server is 2.34.
+$ sudo sed -i.bak s/\$releasever/8/ /etc/yum.repos.d/docker-ce.repo
 
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
