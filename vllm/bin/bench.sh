a5d2846bbcc530d979a50dc1b3b905188c5ce341
Al Zee <z@alz.ee>
2026-09-22 14:52:45 +0800

    nth

--- vllm/bin/bench.sh
+++ vllm/bin/bench.sh
@@ -4,11 +4,12 @@
 
 # . /s/ascend/vllm/ENVs
 
-export VLLM_USE_MODELSCOPE=true
-
 host=${1:-127.0.0.1}
 port=${2:-8006}
-model_name=${3,,}
+model_path=${3:-/s/modelscope/Eco-Tech/DeepSeek-V4-Flash-w8a8-mtp}
+model_name=$(basename $MODEL_PATH)
+model_name=${model_name,,}
+key="$4"
 
 # see https://docs.vllm.ai/en/latest/cli/bench/serve/
 # --ignore-eos
@@ -16,6 +17,7 @@ model_name=${3,,}
 # --header "Authorization=Bearer sk-xxx" \
 vllm bench serve \
     --served-model-name $model_name \
+    --model $model_path \
     --host $host \
     --port $port \
     --backend openai-chat \
@@ -27,5 +29,6 @@ vllm bench serve \
     --random-input-len 16000 \
     --random-output-len 2048 \
     --num-warmups 10 \
+    --header "Authorization=Bearer $key" \
     --save-result \
     --result-dir /s/public/bench/
